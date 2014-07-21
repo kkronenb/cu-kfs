b1d4978b309db7b906a3f488dddf7e9b901d4a1f
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-07-21 15:11:17 -0400

    Trying again.

---
+++ src/main/java/edu/cornell/kfs/coa/businessobject/SubObjectCodeExtendedAttribute.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package edu.cornell.kfs.coa.businessobject;
+
+import org.kuali.kfs.sys.KFSPropertyConstants;
+import org.kuali.kfs.sys.context.SpringContext;
+import org.kuali.rice.krad.service.BusinessObjectService;
+
+import edu.cornell.kfs.sys.businessobject.YearEndPersistableBusinessObjectExtensionBase;
+
+/**
+ * @author kco26
+ *
+ */
+public class SubObjectCodeExtendedAttribute extends YearEndPersistableBusinessObjectExtensionBase {
+
+
+	/**
+	 * 
+	 */
+    private String subObjectCodeDescr;
+        
+    
+    /*
+     * Class constructor
+     */
+    public SubObjectCodeExtendedAttribute() {
+    	
+    }
+    
+	/**
+	 * @return the subObjectCodeDescr
+	 */
+	public String getSubObjectCodeDescr() {
+		return subObjectCodeDescr;
+	}
+	
+	/**
+	 * @param subObjectCodeDescr the subObjectCodeDescr to set
+	 */
+	public void setSubObjectCodeDescr(String subObjectCodeDescr) {
+		this.subObjectCodeDescr = subObjectCodeDescr;
+	}
+		
+}
