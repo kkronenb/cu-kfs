d2eb00fceb0d077094aa2c9d0eebebfa8304eb51
sdm86 <sdm86@cornell.edu>
2014-07-23 17:01:53 -0400

    added missing imports

--- src/test/java/edu/cornell/kfs/vnd/businessobject/lookup/CuVendorLookupableHelperServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/vnd/businessobject/lookup/CuVendorLookupableHelperServiceImplTest.java
@@ -1,12 +1,18 @@
 package edu.cornell.kfs.vnd.businessobject.lookup;
 
+import org.kuali.kfs.sys.ConfigureContext;
+import org.kuali.kfs.sys.context.KualiTestBase;
+import org.kuali.kfs.sys.context.SpringContext;
+
 @ConfigureContext
 public class CuVendorLookupableHelperServiceImplTest extends KualiTestBase{
 
+	private VendorLookupableHelperService vendorLookupableHelperService;
+	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		
+		vendorLookupableHelperService = SpringContext.getBean(VendorLookupableHelperService.class);
 	}
 	
 	public void testGetSearchResults() {
