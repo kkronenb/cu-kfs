28876dd9bfd6bd382c2512059b0f0e1af25b7355
sdm86 <sdm86@cornell.edu>
2014-07-28 16:45:59 -0400

    lookupable test

--- src/test/java/edu/cornell/kfs/vnd/businessobject/lookup/CuVendorLookupableHelperServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/vnd/businessobject/lookup/CuVendorLookupableHelperServiceImplTest.java
@@ -1,21 +1,44 @@
 package edu.cornell.kfs.vnd.businessobject.lookup;
 
-import org.kuali.kfs.sys.ConfigureContext;
-import org.kuali.kfs.sys.context.KualiTestBase;
-import org.kuali.kfs.sys.context.SpringContext;
+import static org.kuali.kfs.sys.fixture.UserNameFixture.ccs1;
 
-@ConfigureContext
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.kuali.kfs.sys.ConfigureContext;
+import org.kuali.kfs.sys.businessobject.lookup.LookupableSpringContext;
+import org.kuali.kfs.sys.context.KualiTestBase;
+import org.kuali.kfs.vnd.businessobject.VendorDetail;
+import org.kuali.rice.kns.lookup.LookupableHelperService;
+import org.kuali.rice.krad.bo.BusinessObject;
+import org.kuali.rice.krad.lookup.CollectionIncomplete;
+
+@ConfigureContext(session = ccs1)
 public class CuVendorLookupableHelperServiceImplTest extends KualiTestBase{
 
-	private VendorLookupableHelperService vendorLookupableHelperService;
+	private LookupableHelperService vendorLookupableHelperServiceImpl;
+	private Map<String,String> fieldValues;
 	
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-		vendorLookupableHelperService = SpringContext.getBean(VendorLookupableHelperService.class);
+		vendorLookupableHelperServiceImpl = LookupableSpringContext.getLookupableHelperService("vendorLookupableHelperService");
+		vendorLookupableHelperServiceImpl.setBusinessObjectClass(VendorDetail.class);
 	}
 	
 	public void testGetSearchResults() {
+		Map<String,String> fieldValues = new HashMap<String,String>();
+		fieldValues.put("name", "Anak Inc");
+		CollectionIncomplete<BusinessObject> theSearchResults = (CollectionIncomplete<BusinessObject>) vendorLookupableHelperServiceImpl.getSearchResults(fieldValues);
+		
+		assertTrue(theSearchResults.size() == 693);
+		
+		fieldValues.clear();
+		
+		theSearchResults = (CollectionIncomplete<BusinessObject>) vendorLookupableHelperServiceImpl.getSearchResults(fieldValues);
+		
+		assertTrue(theSearchResults.size()!=0);
 		
 	}
 }
