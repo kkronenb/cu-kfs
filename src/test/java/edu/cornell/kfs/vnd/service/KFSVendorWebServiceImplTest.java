228b5257eda03888691efde4efe577abe33c5a83
sdm86 <sdm86@cornell.edu>
2014-07-30 14:52:31 -0400

    start of test for KFSVendorWebService

--- src/test/java/edu/cornell/kfs/vnd/service/KFSVendorWebServiceImplTest.java
+++ src/test/java/edu/cornell/kfs/vnd/service/KFSVendorWebServiceImplTest.java
@@ -1,5 +1,29 @@
 package edu.cornell.kfs.vnd.service;
 
-public class KFSVendorWebServiceImplTest {
+import org.kuali.kfs.sys.ConfigureContext;
+import org.kuali.kfs.sys.context.KualiTestBase;
+import org.kuali.kfs.sys.context.SpringContext;
 
+import edu.cornell.kfs.vnd.fixture.VendorDetailFixture;
+
+@ConfigureContext
+public class KFSVendorWebServiceImplTest extends KualiTestBase {
+
+	KFSVendorWebService kfsVendorWebService;
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		kfsVendorWebService = new KFSVendorWebServiceImpl();
+	}
+	
+	public void testVendorWebService() {
+		try {
+			kfsVendorWebService.retrieveKfsVendor(VendorDetailFixture.ANAK_INC.vendorName, "test");
+//		kfsVendorWebService.addVendor(vendorName, vendorTypeCode, isForeign, taxNumber, taxNumberType, ownershipTypeCode, isTaxable, isEInvoice, addresses, contacts, phoneNumbers, supplierDiversitys)
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
