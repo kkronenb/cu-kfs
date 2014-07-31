36952556a3279995c57069f7bd8409ac54d83763
danielabalmus <db483@cornell.edu>
2014-07-31 11:22:15 +0100

    KFSUPGRADE-1002 Add unit tests for CuB2BShoppingServiceImpl

---
+++ src/test/java/edu/cornell/kfs/module/purap/fixture/B2BShoppingCartItemFixture.java
@@ -0,0 +1,106 @@
+package edu.cornell.kfs.module.purap.fixture;
+
+import org.kuali.kfs.module.purap.businessobject.B2BShoppingCartItem;
+
+/**
+ * Fixture class for B2BShoppingCartItem.
+ */
+public enum B2BShoppingCartItemFixture {
+
+    B2B_ITEM_USING_VENDOR_ID (
+            "10", // quantity
+            "A0125156", // supplierPartId
+            "1012273985063\1", // supplierPartAuxiliaryId
+            "252.60", // unitPrice
+            "USD", // unitPriceCurrency
+            "01:00 Kensington Microsaver Laptop Lock - security cable lock", // description
+            "EA", // unitOfMeasure
+            "64068F", // manufacturerPartID
+            "Dell", // manufacturerName
+            "5314-0", // externalSupplierId - vendorID
+            "Punchout", // productSource
+            "", // systemProductID
+            //"624007902" // SupplierID-DUNS: value in KFS
+            "12829371", // SupplierID-DUNS: value from SciQuest
+            "4050097", // SupplierID-SystemSupplierID
+            "Cart name" // Cart Name
+    );     
+
+    public String quantity;
+    public String supplierPartId;
+    public String supplierPartAuxiliaryId;
+    public String unitPrice;
+    public String unitPriceCurrency;
+    public String description;
+    public String unitOfMeasure;
+    public String manufacturerPartID;
+    public String manufacturerName;
+    public String externalSupplierId;
+    public String productSource;
+    public String systemProductID;
+    public String duns;
+    public String systemSupplierID;
+    public String cartName;
+    
+    private B2BShoppingCartItemFixture(
+            String quantity,
+            String supplierPartId,
+            String supplierPartAuxiliaryId,
+            String unitPrice,
+            String unitPriceCurrency,
+            String description,
+            String unitOfMeasure,
+            String manufacturerPartID,
+            String manufacturerName,
+            String externalSupplierId,
+            String productSource,
+            String systemProductID,
+            String duns,
+            String systemSupplierID,
+            String cartName
+    ) {
+        this.quantity = quantity;
+        this.supplierPartId = supplierPartId;
+        this.supplierPartAuxiliaryId = supplierPartAuxiliaryId;
+        this.unitPrice = unitPrice;
+        this.unitPriceCurrency = unitPriceCurrency;
+        this.description = description;
+        this.unitOfMeasure = unitOfMeasure;
+        this.manufacturerPartID = manufacturerPartID;
+        this.manufacturerName = manufacturerName;
+        this.externalSupplierId = externalSupplierId;
+        this.productSource = productSource;
+        this.systemProductID = systemProductID;                
+        this.duns = duns;
+        this.systemSupplierID = systemSupplierID;
+        this.cartName = cartName;
+    }
+    
+    /**
+     * Creates a B2BShoppingCartItem from this B2BShoppingCartItemFixture.
+     */
+    public B2BShoppingCartItem createB2BShoppingCartItem() {
+        B2BShoppingCartItem item = new B2BShoppingCartItem();
+        
+        item.setQuantity(quantity);
+        item.setSupplierPartId(supplierPartId);
+        item.setSupplierPartAuxiliaryId(supplierPartAuxiliaryId);
+        
+        item.setUnitPrice(unitPrice);
+        item.setUnitPriceCurrency(unitPriceCurrency);
+        item.setDescription(description);
+        item.setUnitOfMeasure(unitOfMeasure);
+        item.setManufacturerPartID(manufacturerPartID);
+        item.setManufacturerName(manufacturerName);
+        
+        item.addExtrinsic("ExternalSupplierId", externalSupplierId);
+        item.addExtrinsic("Product Source", productSource);
+        item.addExtrinsic("SystemProductID", systemProductID);
+        item.addExtrinsic("CartName", cartName);
+        
+        item.setSupplier("DUNS", duns);
+        item.setSupplier("SystemSupplierID", systemSupplierID);
+        
+        return item;
+    }
+}
