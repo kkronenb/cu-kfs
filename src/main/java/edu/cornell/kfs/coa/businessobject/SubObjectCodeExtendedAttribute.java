ab02d4a071d9696d24a16b42d94038119187d508
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-07-31 10:03:01 -0400

    Work so far.

--- src/main/java/edu/cornell/kfs/coa/businessobject/SubObjectCodeExtendedAttribute.java
+++ src/main/java/edu/cornell/kfs/coa/businessobject/SubObjectCodeExtendedAttribute.java
@@ -3,15 +3,22 @@
  */
 package edu.cornell.kfs.coa.businessobject;
 
-import org.kuali.rice.krad.bo.PersistableBusinessObjectExtensionBase;
+import edu.cornell.kfs.sys.businessobject.YearEndPersistableBusinessObjectExtensionBase;
 
 /**
  * @author kco26
  *
  */
-public class SubObjectCodeExtendedAttribute extends PersistableBusinessObjectExtensionBase {
+public class SubObjectCodeExtendedAttribute extends YearEndPersistableBusinessObjectExtensionBase {
 
-
+    /**
+     * Primary Keys!
+     */
+	private String chartOfAccountsCode;
+	private String accountNumber;
+	private String financialObjectCode;
+	private String financialSubObjectCode;
+	
 	/**
 	 * Version ID
 	 */
@@ -42,6 +49,62 @@ public class SubObjectCodeExtendedAttribute extends PersistableBusinessObjectExt
 	 */
 	public void setSubObjectCodeDescr(String subObjectCodeDescr) {
 		this.subObjectCodeDescr = subObjectCodeDescr;
+	}
+
+	/**
+	 * @return the accountNumber
+	 */
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	/**
+	 * @param accountNumber the accountNumber to set
+	 */
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	/**
+	 * @return the financialObjectCode
+	 */
+	public String getFinancialObjectCode() {
+		return financialObjectCode;
+	}
+
+	/**
+	 * @param financialObjectCode the financialObjectCode to set
+	 */
+	public void setFinancialObjectCode(String financialObjectCode) {
+		this.financialObjectCode = financialObjectCode;
+	}
+
+	/**
+	 * @return the chartOfAccountsCode
+	 */
+	public String getChartOfAccountsCode() {
+		return chartOfAccountsCode;
+	}
+
+	/**
+	 * @param chartOfAccountsCode the chartOfAccountsCode to set
+	 */
+	public void setChartOfAccountsCode(String chartOfAccountsCode) {
+		this.chartOfAccountsCode = chartOfAccountsCode;
+	}
+
+	/**
+	 * @return the financialSubObjectCode
+	 */
+	public String getFinancialSubObjectCode() {
+		return financialSubObjectCode;
+	}
+
+	/**
+	 * @param financialSubObjectCode the financialSubObjectCode to set
+	 */
+	public void setFinancialSubObjectCode(String financialSubObjectCode) {
+		this.financialSubObjectCode = financialSubObjectCode;
 	}
 		
 }
