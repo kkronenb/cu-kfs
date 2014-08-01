f278a8b3b34379719beae846b01f2829a3ef1c71
sdm86 <sdm86@cornell.edu>
2014-08-01 13:51:45 -0400

    fixture work

--- src/test/java/edu/cornell/kfs/vnd/fixture/AddressParameterFixture.java
+++ src/test/java/edu/cornell/kfs/vnd/fixture/AddressParameterFixture.java
@@ -1,21 +1,52 @@
 package edu.cornell.kfs.vnd.fixture;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.kuali.kfs.sys.ConfigureContext;
 
 import edu.cornell.kfs.vnd.service.params.VendorAddressParam;
+import edu.cornell.kfs.vnd.service.params.VendorPhoneNumberParam;
 
 @ConfigureContext
 public enum AddressParameterFixture {
 
-	ONE();
+	ONE(1, "?", "332 Someplace Street", "Somewhereville", "ND", "USA", true, true);
 	
-	private AddressParameterFixture() {
-		
+	public final Integer identifier;
+	public final String vendorAddressTypeCode;
+	public final String vendorLine1Address;
+	public final String vendorCityName;
+	public final String vendorStateCode;
+	public final String vendorCountryCode;
+	public boolean vendorDefaultAddressIndicator;
+	public boolean active;
+	
+	private AddressParameterFixture(Integer identifier, String vendorAddressTypeCode, String vendorLine1Address,
+			String vendorCityName, String vendorStateCode, String vendorCountryCode, boolean defaultAddressIndicator, boolean active) {
+		this.identifier = identifier;
+		this.vendorAddressTypeCode = vendorAddressTypeCode;
+		this.vendorLine1Address = vendorLine1Address;
+		this.vendorCityName = vendorCityName;
+		this.vendorCountryCode = vendorCountryCode;
+		this.vendorStateCode = vendorStateCode;
 	}
+	
 	
 	public VendorAddressParam createVendorAddressParam() {
 		VendorAddressParam vendorAddressParam = new VendorAddressParam();
 		
+		vendorAddressParam.setActive(active);
+		
 		return vendorAddressParam;
 	}
+	
+	public List<VendorAddressParam> getAllFixtures() {
+		ArrayList<VendorAddressParam> allFixtures = new ArrayList<VendorAddressParam>();
+		
+		allFixtures.add(ONE.createVendorAddressParam());
+		
+		return allFixtures;		
+	}
+	
 }
--- src/test/java/edu/cornell/kfs/vnd/service/SupplierDiversityParameterFixture.java
+++ src/test/java/edu/cornell/kfs/vnd/service/SupplierDiversityParameterFixture.java
@@ -1,5 +1,9 @@
 package edu.cornell.kfs.vnd.service;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.kuali.kfs.sys.ConfigureContext;
 
 import edu.cornell.kfs.vnd.service.params.VendorSupplierDiversityParam;
@@ -7,15 +11,36 @@ import edu.cornell.kfs.vnd.service.params.VendorSupplierDiversityParam;
 @ConfigureContext
 public enum SupplierDiversityParameterFixture {
 
-	ONE();
+	DISABLED("DB", new Date("12/12/3030"), true),
+	HUB_ZONE("HZ", new Date("12/12/3030"), true),
+	MINORITY_OWNED("MI", new Date("12/12/3030"), true);
 	
-	private SupplierDiversityParameterFixture () {
-		
+	
+	public final String vendorSupplierDiversityCode;
+	public final Date vendorSupplierDiversityExpirationDate;
+	public boolean active;
+	
+	private SupplierDiversityParameterFixture(String vendorSupplierDiversityCode, Date vendorSupplierDiversityExpirationDate, boolean active) {
+		this.vendorSupplierDiversityCode = vendorSupplierDiversityCode;
+		this.vendorSupplierDiversityExpirationDate = vendorSupplierDiversityExpirationDate;
+		this.active = active;
 	}
 	
 	public VendorSupplierDiversityParam createSupplierDiversityParameter() {
 		VendorSupplierDiversityParam supplierDiversityParameter = new VendorSupplierDiversityParam();
 		
+		supplierDiversityParameter.setVendorSupplierDiversityCode(vendorSupplierDiversityCode);
+		supplierDiversityParameter.setVendorSupplierDiversityExpirationDate(vendorSupplierDiversityExpirationDate);
+		supplierDiversityParameter.setActive(active);
+		
 		return supplierDiversityParameter;
+	}
+	
+	public List<VendorSupplierDiversityParam> getAllFixtures() {
+		ArrayList<VendorSupplierDiversityParam> allFixtures = new ArrayList<VendorSupplierDiversityParam>();
+		
+		allFixtures.add(ONE.createSupplierDiversityParameter());
+		
+		return allFixtures;
 	}
 }
